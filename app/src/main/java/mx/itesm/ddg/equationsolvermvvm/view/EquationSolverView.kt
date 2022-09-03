47117028814ef173c257bfcd512628fe84053de2
a01752785 <a01752785@itesm.mx>
2022-09-03 16:44:04 -0500

    Add EquationSolverView

--- app/src/main/java/mx/itesm/ddg/equationsolvermvvm/view/EquationSolverView.kt
+++ app/src/main/java/mx/itesm/ddg/equationsolvermvvm/view/EquationSolverView.kt
@@ -1,12 +1,40 @@
 package mx.itesm.ddg.equationsolvermvvm.view
 
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import mx.itesm.ddg.equationsolvermvvm.R
+import mx.itesm.ddg.equationsolvermvvm.databinding.ActivityMainBinding
+import mx.itesm.ddg.equationsolvermvvm.viewmodel.EquationSolverViewModel
 
 class EquationSolverView : AppCompatActivity() {
+    private val viewModel: EquationSolverViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setObservables()
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+        binding.btnSolveEquation.setOnClickListener {
+            val a: Double = binding.etParameterA.text.toString().toDouble()
+            val b: Double = binding.etParameterB.text.toString().toDouble()
+            val c: Double = binding.etParameterC.text.toString().toDouble()
+            viewModel.solve(a, b, c)
+        }
+    }
+
+    private fun setObservables() {
+        viewModel.root1.observe(this) { root ->
+            binding.etRoot1.setText(root)
+        }
+        viewModel.root2.observe(this) { root ->
+            binding.etRoot2.setText(root)
+        }
     }
 }
